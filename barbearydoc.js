ccf490e1b4634ce3533d237c261455dc9a8d4924
unknown <eliasjrcosta886@gmail.com>
2023-05-29 10:08:52 -0300

    criando carrossel

---
+++ barbearydoc.js
@@ -0,0 +1,24 @@
+var slideIndex = 0;
+showSlide(slideIndex);
+
+function changeSlide(n) {
+  showSlide(slideIndex += n);
+}
+
+function showSlide(n) {
+  var slides = document.getElementsByClassName("ul")[0].getElementsByTagName("img");
+
+  if (n >= slides.length) {
+    slideIndex = 0;
+  }
+
+  if (n < 0) {
+    slideIndex = slides.length - 1;
+  }
+
+  for (var i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slides[slideIndex].style.display = "block";
+}
